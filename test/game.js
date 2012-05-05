a9e534bd55692aeed8c8366c9460daab15933793
Pierre Bertet <bonjour@pierrebertet.net>
2012-05-05 05:32:23 +0200

    Check waiting actions before triggering a new turn
    If a user has no actions, he should send new commands until one is valid.
    TODO: send an error message to the player.

--- test/game.js
+++ test/game.js
@@ -1,26 +1,58 @@
 var assert = require('assert'),
     Game = require('../lib/game'),
-    winston = require('winston');
+    winston = require('winston'),
+    logger = new winston.Logger({
+      transports: [
+        // new winston.transports.Console(),
+      ]
+    }),
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
-describe('game mechanisms', function(){
-  var game, letters;
-
-  var logger = new winston.Logger({
-    transports: [
-      // new winston.transports.Console(),
-    ]
-  });
-
-  game = new Game(1, 'player1@example.com', 'player1@example.com', {
+function mkGame(id) {
+  return new Game(id || 1, 'player1@example.com', 'player2@example.com', {
     logger: logger
   });
-  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+}
+
+describe('game API', function() {
+  var game;
+  beforeEach(function() {
+    game = mkGame(1);
+  });
+
+  describe('commandsEnd', function(){
+    it('should not trigger the next turn if the player has no waiting commands', function(){
+      game.command(game.player1, 'attack J10'); // valid command
+      game.commandsEnd(game.player1);
+      game.commandsEnd(game.player2);
+
+      assert.equal(game.turn, 1);
+      assert(game.player1.wait);
+      assert(!game.player2.wait);
+    });
+
+    it('should trigger the next turn if the player has waiting commands', function(){
+      game.command(game.player1, 'attack J10'); // valid command
+      game.command(game.player2, 'attack J10'); // valid command
+      game.commandsEnd(game.player1);
+      game.commandsEnd(game.player2);
+
+      assert.equal(game.turn, 2);
+      assert(!game.player1.wait);
+      assert(!game.player2.wait);
+    });
+  });
+});
+
+describe('game mechanisms', function(){
+  var game = mkGame(2);
 
   describe('attack command', function(){
     it('should remove health points', function(){
       var p2Position = game.terrain.getPosition(game.player2, function(err, x, y) {
         var coords = (y+1) + letters[x]; // 'M16'
         game.command(game.player1, 'attack ' + coords);
+        game.command(game.player2, 'attack A5');
         game.commandsEnd(game.player1);
         game.commandsEnd(game.player2);
         assert(game.player2.health < 40);
