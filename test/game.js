beb0ac719eb96410acdbadede1ce1059fcd3c8c1
Pierre Bertet <bonjour@pierrebertet.net>
2012-05-05 03:44:47 +0200

    Game: cast -> attack
    - Tests
    - Better logging
    - Terrain: new getPosition() method

---
+++ test/game.js
@@ -0,0 +1,34 @@
+var assert = require('assert'),
+    Game = require('../lib/game'),
+    winston = require('winston');
+
+describe('game mechanisms', function(){
+  var game, letters;
+
+  var logger = new winston.Logger({
+    transports: [
+      // new winston.transports.Console(),
+    ]
+  });
+
+  game = new Game(1, 'player1@example.com', 'player1@example.com', {
+    logger: logger
+  });
+  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  describe('attack command', function(){
+    it('should remove health points', function(){
+      var p2Position = game.terrain.getPosition(game.player2, function(err, x, y) {
+        var coords = (y+1) + letters[x]; // 'M16'
+        game.command(game.player1, 'attack ' + coords);
+        game.commandsEnd(game.player1);
+        game.commandsEnd(game.player2);
+        assert.equal(game.player2.health, 35);
+      });
+
+      // game.command(game.player1, 'attack d3');
+
+    });
+  });
+
+});
